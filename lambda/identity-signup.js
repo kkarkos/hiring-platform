cf921efe7e34590a3055f899f6cd0422b9ea5914
Konrad <konrad@krad.rocks>
2020-03-04 15:08:15 +1100

    lamda change

--- lambda/identity-signup.js
+++ lambda/identity-signup.js
@@ -1,15 +1,14 @@
-export function handler(event, context, callback) {
+module.exports.handler = (event, context, callback) => {
   const payload = JSON.parse(event.body);
 
-  console.log('payload', payload);
-  // const user = payload.user;
+  const user = payload.user;
 
   // // this fires for only email+password signups, not for signups via external providers e.g. Google/GitHub
 
-  // console.log('User signed up', user);
+  console.log('User signed up', user);
 
   callback(null, {
     statusCode: 200,
-    body: { user: 'null' },
+    body: user,
   });
-}
+};
