1ed7f7e951b9ec614b55d049224ba127de9f161a
Konrad <konrad@krad.rocks>
2020-03-04 15:05:50 +1100

    move function

--- lambda/identity-signup.js
+++ lambda/identity-signup.js
@@ -1,13 +1,15 @@
 export function handler(event, context, callback) {
   const payload = JSON.parse(event.body);
-  const user = payload.user;
 
-  // this fires for only email+password signups, not for signups via external providers e.g. Google/GitHub
+  console.log('payload', payload);
+  // const user = payload.user;
 
-  console.log('User signed up', user);
+  // // this fires for only email+password signups, not for signups via external providers e.g. Google/GitHub
+
+  // console.log('User signed up', user);
 
   callback(null, {
     statusCode: 200,
-    body: user,
+    body: { user: 'null' },
   });
 }
